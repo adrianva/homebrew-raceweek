e8512fd21953262a62b288d45e0c772ed215f3b0
Adrián Vázquez <adrian.vazquez.alvarez@mrmilu.com>
2026-02-17 16:11:03 +0100

    Add raceweek cask for beta distribution

---
+++ Casks/raceweek.rb
@@ -0,0 +1,17 @@
+cask "raceweek" do
+  version "0.1.0-beta1"
+  sha256 "REPLACE_WITH_SHA256"
+
+  url "https://github.com/adrianva/RaceWeek/releases/download/v#{version}/RaceWeek-v#{version}.dmg"
+  name "RaceWeek"
+  desc "Menu bar app + widget for Formula 1 weekend schedule and standings"
+  homepage "https://github.com/adrianva/RaceWeek"
+
+  app "RaceWeek.app"
+
+  zap trash: [
+    "~/Library/Application Support/RaceWeek",
+    "~/Library/Caches/com.adrianva.RaceWeek",
+    "~/Library/Preferences/com.adrianva.RaceWeek.plist",
+  ]
+end
