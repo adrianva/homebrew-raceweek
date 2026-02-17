a515340e87853d9c3586a8f228f6e866cf91a2fa
Adrián Vázquez <adrian.vazquez.alvarez@mrmilu.com>
2026-02-17 17:07:44 +0100

    Point cask URL to public homebrew-raceweek release assets

--- Casks/raceweek.rb
+++ Casks/raceweek.rb
@@ -2,7 +2,7 @@ cask "raceweek" do
   version "0.1.0-beta1"
   sha256 "dad22f3a98e4d497f30ae254b42c4cad8d939f35a03aea8f5e8bdd2bab2ab814"
 
-  url "https://github.com/adrianva/RaceWeek/releases/download/v#{version}/RaceWeek-v#{version}.dmg"
+  url "https://github.com/adrianva/homebrew-raceweek/releases/download/v#{version}/RaceWeek-v#{version}.dmg"
   name "RaceWeek"
   desc "Menu bar app + widget for Formula 1 weekend schedule and standings"
   homepage "https://github.com/adrianva/RaceWeek"
