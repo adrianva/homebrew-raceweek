b44c608c350f3de413a2abcd97011f8a20494e17
Adrián Vázquez <adrian.vazquez.alvarez@mrmilu.com>
2026-02-17 16:14:34 +0100

    Update cask for v0.1.0-beta1 release

--- Casks/raceweek.rb
+++ Casks/raceweek.rb
@@ -1,6 +1,6 @@
 cask "raceweek" do
   version "0.1.0-beta1"
-  sha256 "REPLACE_WITH_SHA256"
+  sha256 "dad22f3a98e4d497f30ae254b42c4cad8d939f35a03aea8f5e8bdd2bab2ab814"
 
   url "https://github.com/adrianva/RaceWeek/releases/download/v#{version}/RaceWeek-v#{version}.dmg"
   name "RaceWeek"
