b399773e4cd3737c35429e7576aeb36537e1259b
Adrián Vázquez <adri.forzaferrari@gmail.com>
2026-02-19 22:57:19 +0100

    Update raceweek cask to 0.1.0-beta7

--- Casks/raceweek.rb
+++ Casks/raceweek.rb
@@ -1,6 +1,6 @@
 cask "raceweek" do
-  version "0.1.0-beta6"
-  sha256 "bdccb221f1c26c934abbfb613b5933cf808af2ea735e12830d67aa674a7834bf"
+  version "0.1.0-beta7"
+  sha256 "5bcea0246154ad9a9b85a9dc6cd167f6f5b87ceb61002ef7b08a00fcb399a293"
 
   url "https://github.com/adrianva/homebrew-raceweek/releases/download/v#{version}/RaceWeek-v#{version}.dmg"
   name "RaceWeek"
