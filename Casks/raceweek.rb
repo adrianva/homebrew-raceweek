e403fc2bee586796e29e17a9346141893319fd05
Adrián Vázquez <adri.forzaferrari@gmail.com>
2026-02-23 22:15:33 +0100

    chore(cask): release raceweek 0.1.0-beta12

--- Casks/raceweek.rb
+++ Casks/raceweek.rb
@@ -1,8 +1,8 @@
 cask "raceweek" do
-  version "0.1.0-beta11"
+  version "0.1.0-beta12"
   sha256 "0ace4191d6a1fd11c75690e77e52005c4fcd15b033d48bab2b1ffbe5f92de922"
 
-  url "https://github.com/adrianva/homebrew-raceweek/releases/download/v#{version}/RaceWeek-v#{version}.dmg"
+  url "https://github.com/adrianva/RaceWeek/releases/download/v#{version}/RaceWeek-v#{version}.dmg"
   name "RaceWeek"
   desc "Menu bar app + widget for Formula 1 weekend schedule and standings"
   homepage "https://github.com/adrianva/RaceWeek"
